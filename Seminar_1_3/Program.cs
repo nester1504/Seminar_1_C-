2bba08fc376b00d2e0e11ae025f46e02256d8fac
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-11 01:11:54 +0600

    Seminar 1/3

---
+++ Seminar_1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число №1 ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negative = N * (-1);
+while (negative <= N)
+{
+    Console.Write(negative + " ");
+    negative++;
+}
